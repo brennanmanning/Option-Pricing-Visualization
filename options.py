cc6a205936f6455e0f69cab6e5a993321d0e3616
Brennan Manning <manningb24@gmail.com>
2021-12-27 22:36:27 -0500

    Added a sample path plot generator

--- options.py
+++ options.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class GeometricBrownianMotion:
 
@@ -17,13 +17,36 @@ class GeometricBrownianMotion:
     def get_initial_value(self):
         return self.initial_val
 
-    def generate_path(self, nr_steps):
-        dt = 1 / nr_steps
-        S = np.empty([nr_steps + 1, 1])
+    def generate_path(self, steps):
+        dt = 1 / steps
+        S = np.empty([steps + 1, 1])
         S[0] = self.initial_val
 
-        for i in range(nr_steps):
+        for i in range(steps):
             S[i+1] = S[i] * np.exp((self.mu - self.sigma**2 / 2) * dt +
                                    self.sigma * np.random.normal(0, np.sqrt(dt)))
 
         return S
+
+    def plot_sample_paths(self, samples, steps):
+        S = np.empty([steps + 1, samples])
+
+        for j in range(samples):
+            S[j,:] = self.generate_path(steps)
+
+        t = np.linspace(0, 1, num = steps + 1)
+        fig, ax = plt.subplots()
+        ax.plot(t, S, color= "#ffffff", alpha = 0.01)
+        return ax
+        
+
+# Example Usage
+
+# GBM with drift 0 and volatility 0.1 that starts at 1
+gbm = GeometricBrownianMotion(0, 0.1, 1)
+
+# Sample Paths can be generated as follows
+path1 = gbm.generate_path(100)
+path2 = gbm.generate_path(10)
+
+gbm_plot = gbm.plot_sample_paths(1000, 100)
